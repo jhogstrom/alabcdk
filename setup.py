1fc1d26db3cc1df5670dcf6fb90a8ea1ac0fc731
Jesper Högström <jesper.hogstrom@aditrologistics.com>
2021-08-23 11:39:09 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from os import path
+from setuptools import find_packages, setup
+
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(here, "requirements.txt")) as f:
+    deps = [l.strip() for l in f.readlines() if l.strip() != ""]
+
+setup(name='aws',
+    version='1.0.0.0',
+    description='Useful CDK constructs',
+    # url='http://github.com/storborg/funniest',
+    author='Jesper Högström',
+    author_email='jesper.hogstrom@aditrologistics.com',
+    # license='MIT',
+    packages=find_packages(),
+    # zip_safe=False
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    install_requires=deps,
+    package_data={'': ['preinstalled*.txt']},
+    include_package_data=True,
+)

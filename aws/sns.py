199b99b0d2bde2142fc64d7578c309c35f0d7298
Jesper Hogstrom <jesper.hogstrom@aditrologistics.com>
2022-02-28 18:29:56 +0100

    Improved the (SNS)Topic-contruct
    It now
    * supports publishers and subscribers.
    * Adding env-vars to pub and sub that are functions.

--- aws/sns.py
+++ aws/sns.py
@@ -1,19 +1,28 @@
-from typing import Sequence
-from .utils import (gen_name, get_params)
+from typing import Sequence, List
+from .utils import (gen_name)
 from constructs import Construct
 from aws_cdk import (
     aws_sns,
     aws_sns_subscriptions,
+    aws_iam,
     aws_lambda)
+import aws_cdk as cdk
 
 
 class Topic(aws_sns.Topic):
+    def update_environment(self, env_var_name: str, receivers: List):
+        for receiver in receivers:
+            if isinstance(receiver, aws_lambda.Function):
+                receiver.add_environment(env_var_name, self.topic_arn)
+
     def __init__(
             self,
             scope: Construct,
             id: str,
             *,
+            env_var_name: str = None,
             subscribers: Sequence[aws_lambda.Function] = None,
+            publishers: Sequence[aws_iam.IGrantable] = None,
             **kwargs):
         """
         Creates a Topic and optionally adds lambda subscribers.
@@ -23,7 +32,15 @@ class Topic(aws_sns.Topic):
         """
         kwargs.setdefault('topic_name', gen_name(scope, id))
         subscribers = subscribers or []
+        publishers = publishers or []
 
         super().__init__(scope, id, **kwargs)
+        env_var_name = env_var_name or id
         for fn in subscribers:
             self.add_subscription(aws_sns_subscriptions.LambdaSubscription(fn))
+        for grantee in publishers:
+            self.grant_publish(grantee)
+
+        self.update_environment(env_var_name, subscribers)
+        self.update_environment(env_var_name, publishers)
+        cdk.CfnOutput(self, f"{env_var_name}", value=self.topic_arn)

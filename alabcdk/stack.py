4a47b9d1964142144744a9c6e4edefca593f63db
Jesper Hogstrom <jesper.hogstrom@aditrologistics.com>
2022-06-09 16:00:07 +0200

    v1.1.7 - Adding git info to stack on deploy

--- alabcdk/stack.py
+++ alabcdk/stack.py
@@ -2,9 +2,39 @@ from .utils import (generate_output)
 from constructs import Construct
 from aws_cdk import (
     Stack)
+import subprocess
+from typing import List
 
 
 class AlabStack(Stack):
+    def execute(self, cmd: str) -> str:
+        with subprocess.Popen(cmd.split(), stdout=subprocess.PIPE) as proc:
+            res = proc.stdout.read().decode().strip().replace("\t", " ").split("\n")
+        return res
+
+    def git_commit_id(self) -> str:
+        return self.execute("git log --format='%H' -n 1")[0][1:-1]
+
+    def git_remotes(self) -> List[str]:
+        res = self.execute("git remote -v")
+        return res
+
+    def git_tag(self) -> str:
+        return self.execute("git describe --always")[0][1:-1]
+
+    def git_branch(self) -> str:
+        return self.execute("git branch --show-current")[0]
+
+    def add_deploy_info(self, add_git_info: bool) -> None:
+        generate_output(self, "STAGE", self.stage)
+        generate_output(self, "USER", self.user)
+        if add_git_info:
+            generate_output(self, "git_id", self.git_commit_id())
+            generate_output(self, "git_tag", self.git_tag())
+            generate_output(self, "git_branch", self.git_branch())
+            for i, remote in enumerate(self.git_remotes()):
+                generate_output(self, f"git_remote_{i}", remote)
+
     def __init__(
             self,
             scope: Construct,
@@ -12,12 +42,12 @@ class AlabStack(Stack):
             *,
             stage: str = None,
             user: str = None,
+            add_git_info: bool = True,
             **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         self.stage = stage or "DEV"
         self.user = user or "None"
-        generate_output(self, "STAGE", self.stage)
-        generate_output(self, "USER", self.user)
+        self.add_deploy_info(add_git_info)
 
     @property
     def hosted_zone(self):
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, "requirements.txt")) as f:
 
 setup(
     name='alabcdk',
-    version='1.1.6',
+    version='1.1.7',
     description='Useful CDK constructs',
     url='https://github.com/aditrologistics/alabcdk.git',
     author='Jesper Högström',

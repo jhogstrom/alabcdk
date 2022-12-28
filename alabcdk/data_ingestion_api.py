9ef60e8d0ae02789c13ed6772eda3c59a50ba0d0
Erik Lundevall Zara <45368867+eriklz@users.noreply.github.com>
2022-12-28 14:58:59 +0100

    feat: add initial version for custom domain mapping for data ingestion API.

--- alabcdk/data_ingestion_api.py
+++ alabcdk/data_ingestion_api.py
@@ -1,23 +1,77 @@
+from typing import Optional
 from aws_cdk import (
+    Stack,
+    aws_certificatemanager as acm,
     aws_lambda as lambda_,
+    aws_route53 as route53,
     aws_apigatewayv2_alpha as api_gw2,
     aws_apigatewayv2_authorizers_alpha as _authorizers,
     aws_apigatewayv2_integrations_alpha as _api_integrations
 )
 from constructs import Construct
+from .utils import (gen_name, get_params, filter_kwargs, generate_output)
+
+
+class ApiDomain(Construct):
+    def __init__(self, scope: Construct, id: str, *,
+                 domain_name: str,
+                 zone_name: str,
+                 hosted_zone_id: str,
+                 certificate: Optional[acm.ICertificate] = None,
+                 region: Optional[str] = None,
+                 **kwargs) -> None:
+        super().__init__(scope, id)
+
+        self.name = domain_name
+        self.hosted_zone = route53.HostedZone.from_hosted_zone_attributes(
+            self,
+            gen_name(self, "hosted_zone"),
+            hosted_zone_id=hosted_zone_id,
+            zone_name=zone_name
+        )
+
+        self.certificate = certificate or acm.DnsValidatedCertificate(
+            self,
+            gen_name(self, "cert"),
+            hosted_zone=self.hosted_zone,
+            domain_name=domain_name,
+            region=region or Stack.of(self).region
+        )
+
+        self.domain_name = api_gw2.DomainName(
+            self,
+            gen_name(self, "dn"),
+            domain_name=domain_name,
+            certificate=self.certificate
+        )
+
+    def get_domain_mapping_options(self, mapping_key: Optional[str] = None) -> api_gw2.DomainMappingOptions:
+        return api_gw2.DomainMappingOptions(domain_name=self.domain_name, mapping_key=mapping_key)
+
 
 class DataIngestionApi(Construct):
     def __init__(self, scope: Construct, construct_id: str, *,
                  name: str,
-                 description: str) -> None:
+                 description: str,
+                 api_domain: ApiDomain = None,
+                 domain_mapping_key: str = None) -> None:
         super().__init__(scope, construct_id)
+
+        if api_domain is not None:
+            domain_mapping = api_domain.get_domain_mapping_options(domain_mapping_key)
+        else:
+            domain_mapping = None
 
         self._api = api_gw2.HttpApi(self, 'api',
                                     api_name=name,
-                                    description=description)
-
-    def add_stage(self, stage_name: str) -> None:
-        self._api.add_stage(f'stage-{stage_name}', stage_name=stage_name)
+                                    description=description,
+                                    default_domain_mapping=domain_mapping,
+                                    disable_execute_api_endpoint=False)
+        if api_domain is not None:
+            self.url = self._api.default_stage.domain_url
+        else:
+            self.url = self._api.default_stage.url
+        generate_output(self, f"{id}_url", self.url)
 
     def add_ingestion_path_new_data(self, path: str,
                                     integration_fn: lambda_.IFunction,
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, "requirements.txt")) as f:
 
 setup(
     name='alabcdk',
-    version='1.2.0',
+    version='1.2.06',
     description='Useful CDK constructs',
     url='https://github.com/aditrologistics/alabcdk.git',
     author='Jesper Högström',
